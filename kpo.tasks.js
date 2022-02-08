d18e89f8c60512e0e6c7c77321e7526e5cb87594
Rafa Mel <melrcm@gmx.net>
2022-02-08 20:01:30 +0000

    add root kpo tasks

---
+++ kpo.tasks.js
@@ -0,0 +1,8 @@
+module.exports = ({ exec }) => ({
+  'build:e5': exec('pio', ['run', '-e', 'melzi_optiboot_optimized'], {
+    cwd: 'ender-5'
+  }),
+  'build:b1': exec('pio', ['run', '-e', 'BIGTREE_SKR_2'], {
+    cwd: 'b1-se'
+  })
+});
