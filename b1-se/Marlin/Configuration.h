e303359efb2328d7988e355e7ca9383fd508f096
Rafa Mel <melrcm@gmx.net>
2022-02-13 12:25:02 +0000

    b1-se: disable PIDTEMPBED

--- b1-se/Marlin/Configuration.h
+++ b1-se/Marlin/Configuration.h
@@ -662,8 +662,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-/** Alter, Printer Setup */
-#define PIDTEMPBED
+//#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -681,12 +680,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  /** Alter, Printer Setup */
-  #define DEFAULT_bedKp 115.53
-  /** Alter, Printer Setup */
-  #define DEFAULT_bedKi 22.22
-  /** Alter, Printer Setup */
-  #define DEFAULT_bedKd 400.5
+  #define DEFAULT_bedKp 10.00
+  #define DEFAULT_bedKi .023
+  #define DEFAULT_bedKd 305.4
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
